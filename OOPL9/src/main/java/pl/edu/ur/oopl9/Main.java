b5e033ab3cf839fcc36c0ec642a6f5977b01490e
alexanderszelest <37311015+alexanderszelest@users.noreply.github.com>
2018-05-15 15:36:41 +0200

    Add files via upload

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -3,19 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pl.edu.ur.oopl9;
+package javaapplication16;
+
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  *
- * @author maarchyl
+ * @author xszel
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner skan = new Scanner(System.in);
+        HashMap<String,String> slownik;
+        slownik = new HashMap<>();
+        slownik.put("yes ", "tak");
+        slownik.put("no", "nie");
+        slownik.put("perhaps", "może ");
+        slownik.put("please", "proszę");
+        slownik.put("thanks", "dzięki");
+        slownik.put("hi", "hej");
+        slownik.put("hello", "cześć");
+        slownik.put("goodbye", "do widzenia");
+        slownik.put("goodnight", "dobranoc");
+        slownik.put("excuse me", "przepraszam ");
+        slownik.put("no problem", "nie ma problemu");
+        slownik.put("I understand", "	rozumiem");
+        slownik.put("I don't understand", "nie rozumiem");
+        slownik.put("I know", "wiem");
+        slownik.put(" don't know", "nie wiem");
+        slownik.put("Entrance", "Wejście");
+        slownik.put("Exit", "Wyjście");
+        slownik.put("Push", "Pchać");
+        slownik.put("No entry", "Wstęp wzbroniony");
+        slownik.put("Private", "Prywatne");
+        String xd;
+        while(true){
+            xd=skan.next();
+            if(xd.equals("koniec!")){
+                break;
+            }
+            System.out.println(slownik.get(xd));
+        }
+
+
     }
-    
+
 }
